903bed6e607e514d66e7aa16b61e2b8c7d1bcc90
Iskren <iskren.dev@gmail.com>
2022-11-15 23:02:47 +0100

    Broken component removed

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AvatarComponent } from '../src/components/avatar'
-
-export default function Wrapper({ }) {
-    return (
-        <AvatarComponent/>
-    )
-  }
-  
