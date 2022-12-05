0954ff682479514fd1f27f581f224cd4adc5511f
Iskren <iskren.dev@gmail.com>
2022-12-05 18:15:21 +0100

    ci: second attempt to assign next-env to nextra cofig by object destructuring

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withNextEnv = require('next-env');
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
-const app = Object.assign(withNextra({
-  i18n: {
-    locales: ['en', 'bg', 'nl'],
-    defaultLocale: 'en',
-  },
-}), withNextEnv());
+const app = {
+	...withNextEnv(),
+	...withNextra({
+		i18n: {
+			locales: ['en', 'bg', 'nl'],
+			defaultLocale: 'en',
+		},
+	}),
+};
 
 module.exports = app;
