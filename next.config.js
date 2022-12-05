527cd607b0317ab33865dd2233f6c05dba47787f
Iskren <iskren.dev@gmail.com>
2022-12-05 17:09:24 +0100

    ci: first attemt to access .env variable within vercel preview environment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const withNextEnv = require('next-env');
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+
 module.exports = withNextra({
     i18n: {
         locales: ['en', 'bg', 'nl'],
         defaultLocale: 'en',
     },
 })
+
+module.exports = withNextEnv();
